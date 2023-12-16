e7e9efd08abdc22f311f2f940dc61fb70fdc9c23
Chaosbear <sukrit.chatmeeboon@gmail.com>
2023-12-16 16:36:03 +0700

    Initial Commit

---
+++ DevExIOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DevExIOS
+//
+//  Created by Sukrit Chatmeeboon on 16/12/2566 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
